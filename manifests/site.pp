48da79d88ae058c6a0bc544150f2dd8271c32cbd
Cole White <cwhite@wikimedia.org>
2018-09-21 14:34:04 -0600

    further build out of the icinga module and begin integrations

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,4 +1,20 @@
 
+$site = $facts['ipaddress'] ? {
+  /^208\.80\.15[23]\./                                      => 'codfw',
+  /^208\.80\.15[45]\./                                      => 'eqiad',
+  /^10\.6[48]\./                                            => 'eqiad',
+  /^10\.19[26]\./                                           => 'codfw',
+  /^91\.198\.174\./                                         => 'esams',
+  /^198\.35\.26\./                                          => 'ulsfo',
+  /^10\.128\./                                              => 'ulsfo',
+  /^10\.20\.0\./                                            => 'esams',
+  /^103\.102\.166\./                                        => 'eqsin',
+  /^10\.132\./                                              => 'eqsin',
+  /^172\.16\.([0-9]|[1-9][0-9]|1([0-1][0-9]|2[0-7]))\./     => 'eqiad',
+  /^172\.16\.(1(2[8-9]|[3-9][0-9])|2([0-4][0-9]|5[0-5]))\./ => 'codfw',
+  default                                                   => '(undefined)'
+}
+
 # Vagrant puppetmaster
 node 'puppet.test' {
   include base
@@ -7,7 +23,6 @@ node 'puppet.test' {
 }
 
 node default {
-  include base
 
   $pieces = split($trusted['certname'], '[.]')
 
@@ -19,6 +34,7 @@ node default {
     }
     2: {
       $prefix = $pieces[0]
+      $datacenter = $pieces[1]
       $tld = $pieces[1]
     }
     default: {
